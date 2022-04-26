f1f01061307eaf42c5cff6c240d121f08ac4f33d
Laiicha <77719155+Laiicha@users.noreply.github.com>
2022-04-26 23:59:18 +0200

    Update Oiseau.hpp

--- Oiseau.hpp
+++ Oiseau.hpp
@@ -8,31 +8,31 @@
 class Oiseau
 {
 public:
-  Vecteur position;
-  Vecteur vitesse;
-  Vecteur acceleration;
-  double vitesse_max;
-	
-	Vecteur seek(const Vecteur& v);
-  void run(const vector<Oiseau>& v);
-  void update();
-  void flock(const vector<Oiseau>& v);
-  void borders();
-  double angle(const Vecteur& v);
+  
+	Vecteur position;  
+	Vecteur vitesse;  
+	Vecteur acceleration;
+	double vitesse_max;	
+	Vecteur seek(const Vecteur& v);  
+	void run(const vector<Oiseau>& v);  
+	void update();  
+	void flock(const vector<Oiseau>& v);  
+	void borders();  
+	double angle(const Vecteur& v);
 };
 class Proie : public Oiseau
 {
 public:
-  Vecteur Alignement(const Vecteur<Proie>& Proie);
+	Vecteur Alignement(const Vecteur<Proie>& Proie);
 	Vecteur Separation(const Vecteur<Proie>& Proie);
-  Vecteur Cohesion(const Vecteur<Proie>& Proie);	
+	Vecteur Cohesion(const Vecteur<Proie>& Proie);	
 	Vecteur Dispersion(const Vecteur<Proie>& Proie);
 	Vecteur Total(const Vecteur<Proie>& Proie);
 };
 class Predateur : public Oiseau
 {
 public:
-  Vecteur Attraction(const Vecteur<Predateur>& Predateur);
+	Vecteur Attraction(const Vecteur<Predateur>& Predateur);
 	Vecteur Repulsion(const Vecteur<Predateur>& Predateur);
 };
 
