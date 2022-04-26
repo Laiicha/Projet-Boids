9f1490f9355c507c858cc7360eec2f4c9f676890
Laiicha <77719155+Laiicha@users.noreply.github.com>
2022-04-26 10:58:19 +0200

    Update oiseaux.hpp

--- oiseaux.hpp
+++ oiseaux.hpp
@@ -1,1 +1,24 @@
+#include <vector>
+#include <iostream>
+#include <stdlib.h>
 
+#ifndef _OISEAUX_HPP_
+#define _OISEAUX_HPP_
+class Oiseaux  
+{
+public:
+  // bool predator;
+  Vecteurs position;
+  Vecteurs vitesse;
+  Vecteurs acceleration;
+  float vitesse_max;
+  Oiseaux
+}
+class Proies
+{
+  
+}
+class Predateurs
+{
+  
+}
