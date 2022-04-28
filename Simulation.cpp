f2ecdb5d6f67f88c175787ff176b35a0d931149f
Laiicha <77719155+Laiicha@users.noreply.github.com>
2022-04-28 20:32:43 +0200

    Update Simulation.cpp

--- Simulation.cpp
+++ Simulation.cpp
@@ -9,14 +9,14 @@
 // Construct window using SFML
 Simulation::Simulation()
 {
-    this->boidsSize = 3.0;
+    this->taille_boid = 3.0;
     sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
-    this->window_height = desktop.height;
-    this->window_width  = desktop.width;
-    this->window.create(sf::VideoMode(window_width, window_height, desktop.bitsPerPixel), "Boids", sf::Style::None);
+    this->longueur_fenetre = desktop.height;
+    this->largeur_fenetre  = desktop.width;
+    this->fenetre.create(sf::VideoMode(largeur_fenetre, longueur_fenetre, desktop.bitsPerPixel), "Boids", sf::Style::None);
     
     // Try to achieve 60 FPS.
-    window.setFramerateLimit(60);
+    fenetre.setFramerateLimit(60);
 }
 
 // Run the simulation. Run creates the boids that we'll display, checks for user
@@ -24,32 +24,33 @@ Simulation::Simulation()
 void Game::Run()
 {
     for (int i = 0; i < 250; i++) {
-        Boid b(window_width / 3, window_height / 3); // Starts all boids in the center of the screen
+       // Boid b(largeur_fenetre/3,longueur_fenetre/3); // Starts all boids in the center of the screen
+        Boid b();
         sf::CircleShape shape(8, 3);
 
         // Changing the Visual Properties of the shape
         // shape.setPosition(b.location.x, b.location.y); // Sets position of shape to random location that boid was set to.
-        shape.setPosition(window_width, window_height); // Testing purposes, starts all shapes in the center of screen.
+        shape.setPosition(largeur_fenetre, longueur_fenetre); // Testing purposes, starts all shapes in the center of screen.
         shape.setOutlineColor(sf::Color(0,255,0));
         shape.setFillColor(sf::Color::Green);
         shape.setOutlineColor(sf::Color::White);
         shape.setOutlineThickness(1);
-        shape.setRadius(boidsSize);
+        shape.setRadius(taille_boid);
 
         // Adding the boid to the flock and adding the shapes to the vector<sf::CircleShape>
-        flock.addBoid(b);
-        shapes.push_back(shape);
+        nuee.ajout_boid(b);
+        forme.push_back(shape);
     }
-    while (window.isOpen()) {
+    while (fenetre.isOpen()) {
         HandleInput();
         Render();
     }
 }
 
-void Game::HandleInput()
+void simulation::HandleInput()
 {
     sf::Event event;
-    while (window.pollEvent(event)) {
+    while (fenetre.pollEvent(event)) {
         // "close requested" event: we close the window
         // Implemented alternate ways to close the window. (Pressing the escape, X, and BackSpace key also close the program.)
         if ((event.type == sf::Event::Closed) ||
@@ -60,14 +61,14 @@ void Game::HandleInput()
             (event.type == sf::Event::KeyPressed &&
              event.key.code == sf::Keyboard::X))
              {
-            window.close();
+            fenetre.close();
         }
     }
 
     // Check for mouse click, draws and adds boid to flock if so.
     if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
         // Gets mouse coordinates, sets that as the location of the boid and the shape
-        sf::Vector2i mouseCoords = sf::Mouse::getPosition(window);
+        sf::Vector2i mouseCoords = sf::Mouse::getPosition(fenetre);
         Boid b(mouseCoords.x, mouseCoords.y, false);
         sf::CircleShape shape(10,3);
 
@@ -77,52 +78,52 @@ void Game::HandleInput()
         shape.setFillColor(sf::Color(255, 0, 0));
         shape.setOutlineColor(sf::Color::White);
         shape.setOutlineThickness(1);
-        shape.setRadius(boidsSize);
+        shape.setRadius(taille_boid);
 
         // Adds newly created boid and shape to their respective data structure
-        flock.addBoid(b);
-        shapes.push_back(shape);
+        nuee.addBoid(b);
+        forme.push_back(shape);
 
         // New Shape is drawn
-        window.draw(shapes[shapes.size()-1]);
+        fenetre.draw(forme[forme.size()-1]);
     }
 }
 
 void Simulation::Render()
 {
-    window.clear();
+    fenetre.clear();
 
     // Draws all of the Boids out, and applies functions that are needed to update.
-    for (int i = 0; i < shapes.size(); i++) {
-        window.draw(shapes[i]);
+    for (int i = 0; i < forme.size(); i++) {
+        fenetre.draw(forme[i]);
 
         //cout << "Boid "<< i <<" Coordinates: (" << shapes[i].getPosition().x << ", " << shapes[i].getPosition().y << ")" << endl;
         //cout << "Boid Code " << i << " Location: (" << flock.getBoid(i).location.x << ", " << flock.getBoid(i).location.y << ")" << endl;
 
         // Matches up the location of the shape to the boid
-        shapes[i].setPosition(flock.getBoid(i).location.x, flock.getBoid(i).location.y);
+        forme[i].setPosition(nuee.get_boid(i).position.x, nuee.get_boid(i).position.y);
 
         // Calculates the angle where the velocity is pointing so that the triangle turns towards it.
-        float theta = flock.getBoid(i).angle(flock.getBoid(i).velocity);
-        shapes[i].setRotation(theta);
+        double theta = nuee.get_boid(i).angle(nuee.get_boid(i).vitesse);
+        forme[i].setRotation(theta);
 
         // Prevent boids from moving off the screen through wrapping
         // If boid exits right boundary
-        if (shapes[i].getPosition().x > window_width)
-            shapes[i].setPosition(shapes[i].getPosition().x - window_width, shapes[i].getPosition().y);
+        if (forme[i].getPosition().x > largeur_fenetre)
+            forme[i].setPosition(forme[i].getPosition().x - largeur_fenetre, forme[i].getPosition().y);
         // If boid exits bottom boundary
-        if (shapes[i].getPosition().y > window_height)
-            shapes[i].setPosition(shapes[i].getPosition().x, shapes[i].getPosition().y - window_height);
+        if (forme[i].getPosition().y > longueur_fenetre)
+            forme[i].setPosition(forme[i].getPosition().x, forme[i].getPosition().y - longueur_fenetre);
         // If boid exits left boundary
-        if (shapes[i].getPosition().x < 0)
-            shapes[i].setPosition(shapes[i].getPosition().x + window_width, shapes[i].getPosition().y);
+        if (forme[i].getPosition().x < 0)
+            forme[i].setPosition(forme[i].getPosition().x + largeur_fenetre, forme[i].getPosition().y);
         // If boid exits top boundary
-        if (shapes[i].getPosition().y < 0)
-            shapes[i].setPosition(shapes[i].getPosition().x, shapes[i].getPosition().y + window_height);
+        if (forme[i].getPosition().y < 0)
+            forme[i].setPosition(forme[i].getPosition().x, forme[i].getPosition().y + longueur_fenetre);
     }
 
     // Applies the three rules to each boid in the flock and changes them accordingly.
-    flock.flocking();
+    nuee.flocking();
 
-    window.display();
+    fenetre.display();
 }
