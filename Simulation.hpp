5ba23ce94e30dcaeff824a56e77c2ae3d64b8c56
Laiicha <77719155+Laiicha@users.noreply.github.com>
2022-04-28 20:32:46 +0200

    Update Simulation.hpp

--- Simulation.hpp
+++ Simulation.hpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include "Nuee.hpp"
+#include "Boid.hpp"
+#include "Vecteur.hpp"
+#include "SFML/Window.hpp"
+#include "SFML/Graphics.hpp"
 
+#ifndef SIMULATION_H
+#define SIMULATION_H
+
+// Game handles the instantiation of a flock of boids, game input, asks the
+// model to compute the next step in the stimulation, and handles all of the
+// program's interaction with SFML. 
+
+class Simulation {
+private:
+    sf::RenderWindow fenetre;
+    int largeur_fenetre;
+    int longueur_fenetre;
+
+    Nuee nuee;
+    float taille_boid;
+    vector<sf::CircleShape> forme;
+
+    void Render();
+    void HandleInput();
+
+public:
+    simulation();
+    void Run();
+};
+
+#endif
